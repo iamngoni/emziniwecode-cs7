da0564abdf275066e0f047d67c30f90d9b0631c7
iamngoni <imngonii@gmail.com>
2021-05-07 16:22:40 +0200

    lab0

---
+++ labs/lab0/lab0.py
@@ -0,0 +1,16 @@
+email = "imngonii@gmail.com" 
+
+def twenty_twenty_one():
+  """Come up with the most creative expression that evaluates to 2020,
+  using only numbers and the +, *, and - operators.
+  
+  >>> twenty_twenty()
+  2021
+  """
+  yearOfBirth = 1998
+  age = 23
+   
+  return (yearOfBirth + age)
+    
+currentYear = twenty_twenty_one()
+print(currentYear)
